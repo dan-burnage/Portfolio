051c1e0b86c5978f0a779a47485d7d65a9b96187
Daniel Burnage <danburnage@Daniels-MacBook-Pro.local>
2020-09-02 00:40:08 +0100

    Housekeeping - WIP and 'weonlyspeakdog' removed

--- index.php
+++ index.php
@@ -1,52 +1,56 @@
 <?php require_once( 'couch/cms.php' ); ?>
 <cms:template title='Portfolio' clonable='1'>
+
     <cms:editable name='project_description' type='richtext' />
     <cms:editable name='project_heroimage' type='image' />
+
     <cms:repeatable name='project_images' >
+
         <cms:editable type='image' name='image' label='Project Images' />
-    </cms:repeatable>
+
+	</cms:repeatable>
+
 </cms:template>
 
 <cms:if k_is_page >
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<title>Dan Burnage</title>
-<link href="css/sitemain.css" rel="stylesheet" type="text/css">
-<link href="css/webkit.scrollbar.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="css/unisanspack/unisanspack.css">
-<script> function myFunction(x) { x.classList.toggle("change"); }</script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Dan Burnage</title>
+	<link href="css/sitemain.css" rel="stylesheet" type="text/css">
+	<link href="css/webkit.scrollbar.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/unisanspack/unisanspack.css">
+	<script> function myFunction(x) { x.classList.toggle("change"); }</script>
 
-<!--
-/**
- * @license
- * MyFonts Webfont Build ID 3074987, 2015-08-13T06:49:36-0400
- *
- * The fonts listed in this notice are subject to the End User License
- * Agreement(s) entered into by the website owner. All other parties are
- * explicitly restricted from using the Licensed Webfonts(s).
- *
- * You may obtain a valid license at the URLs below.
- *
- * Webfont: UniSansBook by Fontfabric
- * URL: http://www.myfonts.com/fonts/font-fabric/uni-sans/book/
- *
- * Webfont: UniSansSemiBold by Fontfabric
- * URL: http://www.myfonts.com/fonts/font-fabric/uni-sans/semi-bold/
- *
- *
- * License: http://www.myfonts.com/viewlicense?type=web&buildid=3074987
- * Licensed pageviews: 20,000
- * Webfonts copyright: Copyright (c) 2014 by &#x00A9; 2009 Designed by Svetoslav Simov
- * &#x00A9; Fontfabric, Inc. All rights reserved.
- *
- * © 2015 MyFonts Inc
-*/
-
--->
-
+	<!--
+	/**
+	* @license
+	* MyFonts Webfont Build ID 3074987, 2015-08-13T06:49:36-0400
+	*
+	* The fonts listed in this notice are subject to the End User License
+	* Agreement(s) entered into by the website owner. All other parties are
+	* explicitly restricted from using the Licensed Webfonts(s).
+	*
+	* You may obtain a valid license at the URLs below.
+	*
+	* Webfont: UniSansBook by Fontfabric
+	* URL: http://www.myfonts.com/fonts/font-fabric/uni-sans/book/
+	*
+	* Webfont: UniSansSemiBold by Fontfabric
+	* URL: http://www.myfonts.com/fonts/font-fabric/uni-sans/semi-bold/
+	*
+	*
+	* License: http://www.myfonts.com/viewlicense?type=web&buildid=3074987
+	* Licensed pageviews: 20,000
+	* Webfonts copyright: Copyright (c) 2014 by &#x00A9; 2009 Designed by Svetoslav Simov
+	* &#x00A9; Fontfabric, Inc. All rights reserved.
+	*
+	* © 2015 MyFonts Inc
+	*/
+	-->
 
 </head>
 
@@ -65,6 +69,7 @@ info@danburnage.com
 </nav>
 
 <div id="contentarea"> <!-- SIBLING #3 -->
+
 	<div id="grid_container">
 
         <div id="grid_desc"><cms:show project_description /></div>
@@ -77,29 +82,34 @@ info@danburnage.com
         <div id="grid_desc">
             <p class="UniSansSemiBold"><a href="index.php">Click here for more work...</a></p>
         </div>
+
         <div id="grid_desc">
             <p class="UniSansSemiBold" style="font-size:12px;">Copyright &copy; 2019 &ndash; Dan Burnage</p>
         </div>
 
     </div>
-	  <div id="oncanvas-menu">
-        <label for="togglebox" style="width: 100%;">
-            <div id="toggle" onclick="myFunction(this)">
-                <div class="bar1"></div><div class="bar2"></div>
-                <div style="color:#3737ff; font-size:12.5px; line-height:30px; text-align:center; letter-spacing:0px;" class="UniSansSemiBold">INFO</div>
-            </div>
-        </label>
-        <a href="index.php" class="logo"><img src="img/logo.svg" class="logoscaler"></a>
-    </div>
-</div>
 
+	<div id="oncanvas-menu">
+
+		<label for="togglebox" style="width: 100%;">
+			<div id="toggle" onclick="myFunction(this)">
+				<div class="bar1"></div><div class="bar2"></div>
+				<div style="color:#3737ff; font-size:12.5px; line-height:30px; text-align:center; letter-spacing:0px;" class="UniSansSemiBold">INFO</div>
+			</div>
+		</label>
+
+		<a href="index.php" class="logo"><img src="img/logo.svg" class="logoscaler"></a>
+	</div>
+
+</div>
 
 </body> <!-- BODY END TAG -->
 
-
 </html>
 <cms:else />
+
     <cms:embed 'list_view.php' />
+
 </cms:if>
 
 <?php COUCH::invoke(); ?>
